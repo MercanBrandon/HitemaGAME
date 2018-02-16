d2da1cb7e0f299e0330881cfb43519e5b5347e7c
Unknown <mercan.brandon@outlook.fr>
2018-02-16 16:46:32 +0100

    16:46

---
+++ src/accueil.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>tpGAME</title>
+    <script src="js/jquery-3.2.1.slim.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+  </head>
+  <body>
+    <div class="container-fluid">
+      <h1>Bienvenue</h1>
+      <h5>Vous êtes :</h5>
+      <a class="btn btn-primary" href="connectUser.php" role="button">Utilisateur</a>
+      <a class="btn btn-success" href="connect.php">Visiteur</a>
+      <a class="btn btn-info" href="connectManagement.php">Manageur</a>
+    </div>
+  </body>
+</html>
